c7c038b96bb38c4f460503bba0aa768324443832
kleyster <aubakirof@gmail.com>
2021-04-07 01:45:18 +0600

    lab 10

---
+++ week10/main/views.py
@@ -0,0 +1,27 @@
+from .models import Category,Product
+from django.http import JsonResponse,HttpResponse
+
+# Create your views here
+def show_products(requests):
+    products = list(Product.objects.values())
+    return JsonResponse(products,safe=False)
+
+def show_product(requests,pid):
+    try:
+        product = list(Product.objects.filter(id=pid).values())
+        return JsonResponse(product,safe=False)
+    except:
+        return HttpResponse("NOT FOUND")
+
+
+def show_categories(request):
+    categories = list(Category.objects.values())
+    return JsonResponse(categories,safe=False)
+
+
+def show_category(request,pid):
+    try:
+        category = list(Category.objects.filter(id=pid).values())
+        return JsonResponse(category,safe=False)
+    except:
+        return HttpResponse("NOT FOUND")
